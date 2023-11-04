ecf75cdd07430bdd17ac4065c06d80d8df7b2c22
James <hawkinsjch@gmail.com>
2023-11-04 18:57:32 -0500

    Added Credit Check Text

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, input } from 'react-native';
+import IsCreditValid from "./approvalFactors";
 
-const TEST_CREDIT_RATING = 600;
+const TEST_CREDIT_RATING = 700;
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Credit Rating: {TEST_CREDIT_RATING}</Text>
-      <Text>Credit Rating Valid: </Text>
+      <Text>Credit Rating Valid: {IsCreditValid(TEST_CREDIT_RATING).toString()}</Text>
       <StatusBar style="auto" />
     </View>
   );
