4febe99cbe405d21843a6a057991bf258e1fb23b
Pacho15 <Oudom.pach@gmail.com>
2023-11-04 18:47:52 -0500

    Update approvalFactors.js

--- approvalFactors.js
+++ approvalFactors.js
@@ -1,27 +1,21 @@
-
-
-
 const CREDIT_MINIMUM = 640;
 
 
-function creditValid(creditRating) {
-    
-    
+function IsCreditValid(creditRating) {
 
-    if (creditRating >= CREDIT_MINIMUM){
-
-        print("Your credit score satisfies the preferred conditions to be approved <3");
-
-    }
-    else{
-        print("Your credit score does NOT satisfy the preffered conditions to be approved");
-    }
-    
-
+    return creditRating >= CREDIT_MINIMUM
   
   }
   
+  function ltv(downPayment , houseAppraisal) {
+
+    
+
+  }
   
-  
-  
+
+
+
+
+export default IsCreditValid;  
   
