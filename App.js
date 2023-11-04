e0ea379ab57258fa8a108c81417b0a3eb8586d36
Pacho15 <Oudom.pach@gmail.com>
2023-11-04 18:35:15 -0500

    credit validation
    checks

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ export default function App() {
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ approvalFactors.js
@@ -0,0 +1,27 @@
+
+
+
+const CREDIT_MINIMUM = 640;
+
+
+function creditValid(creditRating) {
+    
+    
+
+    if (creditRating >= CREDIT_MINIMUM){
+
+        print("Your credit score satisfies the preferred conditions to be approved <3");
+
+    }
+    else{
+        print("Your credit score does NOT satisfy the preffered conditions to be approved");
+    }
+    
+
+  
+  }
+  
+  
+  
+  
+  
