8b93c5562070632ec6f8b39a3696c32cda46265b
James <hawkinsjch@gmail.com>
2023-11-04 22:32:33 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, input } from 'react-native';
 import ApprovalFactors from "./approvalFactors";
-import { parse } from 'csv-parse';
+import Papa from 'papaparse';
+import csvFile from './data/data.csv'
+
 
 const TEST_CREDIT_RATING = 700,
       TEST_DOWN_PAYMENT = 50,
@@ -15,6 +17,7 @@ const TEST_CREDIT_RATING = 700,
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>{ParseCSV()}</Text>
       <Text>Credit Rating: {TEST_CREDIT_RATING}</Text>
       <Text>Credit Rating Valid: {CodeToText(ApprovalFactors.IsCreditValid(TEST_CREDIT_RATING))}</Text>
       <Text>LTV: {CodeToText(ApprovalFactors.LTV(TEST_DOWN_PAYMENT, TEST_HOME_APPRAISAL))}</Text>
@@ -33,6 +36,17 @@ function CodeToText(num) {
     case 2:
       return "Medium"
   }
+}
+
+function ParseCSV(){
+  Papa.parse(csvFile, {
+    download: true,
+    complete: function (input) {
+         const records = input.data;
+         return input.data;
+    }
+});
+
 }
 
 const styles = StyleSheet.create({
