8b77b614ee228436ff95ba0a2530753accebb947
James <hawkinsjch@gmail.com>
2023-11-04 19:17:13 -0500

    Fixed Shit

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, input } from 'react-native';
-import { IsCreditValid, LTV } from "./approvalFactors";
+import ApprovalFactors from "./approvalFactors";
 
 const TEST_CREDIT_RATING = 700;
+const TEST_DOWN_PAYMENT = 100;
+const TEST_HOME_APPRAISAL = 1000;
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Credit Rating: {TEST_CREDIT_RATING}</Text>
-      <Text>Credit Rating Valid: {IsCreditValid(TEST_CREDIT_RATING).toString()}</Text>
+      <Text>Credit Rating Valid: {ApprovalFactors.IsCreditValid(TEST_CREDIT_RATING).toString()}</Text>
+      <Text>LTV: {ApprovalFactors.LTV(TEST_DOWN_PAYMENT, TEST_HOME_APPRAISAL).toString()}</Text>
       <StatusBar style="auto" />
     </View>
   );
